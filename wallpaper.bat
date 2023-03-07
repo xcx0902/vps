07276139cb8fc88b78064e323ba6011c5b62c373
xcx0902 <xcx0902@126.com>
2023-03-07 19:41:12 +0800

    Delete many useless files

--- wallpaper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\WindowsServerFreeVPS\WindowsServerFreeVPS\wallpaper.jpg /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-taskkill /f /im explorer.exe
-start explorer.exe
